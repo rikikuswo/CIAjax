90fbbc5ba914c728a367cc0820ae19a0404788d3
rikikuswo <rikikuswo20@gmail.com>
2021-03-05 15:08:47 +0700

    db

--- db_inventori.sql
+++ db_inventori.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 21 Mei 2017 pada 16.59
--- Versi Server: 5.7.14
--- PHP Version: 5.6.25
+-- Waktu pembuatan: 05 Mar 2021 pada 09.07
+-- Versi server: 10.4.10-MariaDB
+-- Versi PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -37,21 +39,60 @@ CREATE TABLE `tbl_barang` (
 --
 
 INSERT INTO `tbl_barang` (`barang_kode`, `barang_nama`, `barang_harga`) VALUES
+('612768456215', 'Rexona', 1700),
 ('8886057883665', 'Kratindaeng Botol', 5000),
 ('8992388133529', 'Oceana Sea Salt Lemonade', 5000),
 ('8992761136031', 'Sprite 250ml', 8000),
 ('8995227501121', 'Panther Energy Power Red', 4000),
 ('8995227501916', 'Panther Energy Lava Blast', 6000);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `iduser` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `telp` int(11) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`iduser`, `nama`, `telp`, `role`) VALUES
+(1, 'Bang Admin', 810113412, 1),
+(4, 'Xiaomi', 2147483647, 2);
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tbl_barang`
+-- Indeks untuk tabel `tbl_barang`
 --
 ALTER TABLE `tbl_barang`
   ADD PRIMARY KEY (`barang_kode`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
